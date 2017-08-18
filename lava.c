4f3058e315e4f4cd6db8981d3330c257bb7de312
LAVANYAMECH <30549560+LAVANYAMECH@users.noreply.github.com>
2017-08-18 07:21:40 -0400

    lava.c

---
+++ lava.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int N;
+printf("Enter the value of N");
+scanf("%d",&N)
+if(N % 2==0)
+{
+printf("The given number is even");
+}
+else
+{
+printf("the given number is odd");
+}
+}
