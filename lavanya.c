bee08087b8729cc7095b21bb60236944408ad85d
LAVANYAMECH <30549560+LAVANYAMECH@users.noreply.github.com>
2017-08-22 16:02:18 +0530

    Create lavanya.c

---
+++ lavanya.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+inta,b,c;
+printf("Enter the values ofa,b,c");
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c));
+{
+printf("A is a biggest number"); 
+}
+else if(b>c)
+{
+printf("B is biggest number");
+}
+else
+{
+printf("C is biggest number");
+
+
+
+
+
+
